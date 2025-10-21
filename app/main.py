468c1f487e2176614dd576127c9d7c40734f76b4
sher2od <sherzodqarshiboyev543@gmail.com>
2025-10-21 19:37:26 +0300

    railway

--- app/main.py
+++ app/main.py
@@ -1,44 +1,4 @@
 
-
-# from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-
-
-# from app.db import initial_db
-# from app.routers.users import router as user_router
-# from app.routers.cars import router as car_router
-# from app.routers.orders import router as order_router
-# from app.internal.admin import router as admin_router
-# from app.routers.status import router as status_router
-
-
-# app = FastAPI(
-#     title="Rent Car Api",
-#     version="1.0.0",
-#     description="API for managing car rentals"           
-# )
-
-# initial_db()
-
-
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"]
-# )
-
-
-# app.include_router(user_router)
-# app.include_router(car_router)
-# app.include_router(order_router)
-# app.include_router(admin_router)
-# app.include_router(status_router)
-
-
-
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
